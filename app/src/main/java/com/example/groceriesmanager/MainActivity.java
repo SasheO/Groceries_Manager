2e084adec6931a23ca2c7d6cef31467c611ab490
Mezisashe Ojuba <sasheojuba@fb.com>
2022-06-20 15:32:50 -0700

    added groceryListFragment

--- app/src/main/java/com/example/groceriesmanager/MainActivity.java
+++ app/src/main/java/com/example/groceriesmanager/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.groceriesmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,18 +13,65 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.groceriesmanager.Fragments.GroceryListFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.LogOutCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    BottomNavigationView bottomNavigationView;
+    // these lines below are necessary to be able to refer to the fragments from another fragment via the activity
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    public GroceryListFragment groceryListFragment = new GroceryListFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // when you first open the main activity, the grocerylist fragment shows first
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        fragmentManager.beginTransaction().replace(R.id.frameLayout, groceryListFragment);
+        ft.commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.miGroceryList:
+                        // this replaces the fragment housed in frameLayout with a feedfragment
+                        fragment = groceryListFragment;
+                        break;
+                    case R.id.miPantryList:
+                        // this replaces the fragment housed in frameLayout with a postfragment
+//                        fragment = newPostFragment;
+//                        break;
+
+                    case R.id.miSavedList:
+//                        fragment = profileFragment;
+//                        break;
+                    case R.id.miRecipeSearch:
+//                        fragment = profileFragment;
+//                        break;
+                    case R.id.miYoutubeSearch:
+//                        fragment = profileFragment;
+//                        break;
+                    default:
+                        fragment = groceryListFragment;
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit();
+                return true;
+            }
+        });
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
