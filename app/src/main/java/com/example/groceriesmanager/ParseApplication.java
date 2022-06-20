bf9448dcd6b49089248ee5b33b5afea96e8a5d41
Mezisashe Ojuba <sasheojuba@fb.com>
2022-06-20 11:46:49 -0700

    set up parse server

---
+++ app/src/main/java/com/example/groceriesmanager/ParseApplication.java
@@ -0,0 +1,39 @@
+package com.example.groceriesmanager;
+
+import android.app.Application;
+
+import com.example.groceriesmanager.R;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // register all classes that inherit parse
+        // ParseObject.registerSubclass(Post.class);
+        // ParseObject.registerSubclass(Comment.class);
+
+        // Use for monitoring Parse network traffic
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+
+
+        // Set applicationId and server based on the values in the Back4App settings.
+        // todo: set client key and server
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.application_id))
+                .clientKey(getString(R.string.client_key))
+                .clientBuilder(builder)
+                .server("https://parseapi.back4app.com").build());
+    }
+}
