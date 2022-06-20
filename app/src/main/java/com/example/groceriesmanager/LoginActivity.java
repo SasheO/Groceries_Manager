62be561b984cc068384b821c2a16c38b2f524df6
Mezisashe Ojuba <sasheojuba@fb.com>
2022-06-20 13:20:53 -0700

    user can log in

--- app/src/main/java/com/example/groceriesmanager/LoginActivity.java
+++ app/src/main/java/com/example/groceriesmanager/LoginActivity.java
@@ -20,7 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnLogin;
     private EditText etLoginUsername;
     private EditText etLoginPassword;
-    private TextView tvSignUp;
+    private TextView tvRegister;
 
     private static final String TAG = "LoginActivity";
     @Override
@@ -32,15 +32,16 @@ public class LoginActivity extends AppCompatActivity {
         if (ParseUser.getCurrentUser() != null){
             // if user is logged in, go directly to main page
             goToMainActivity();
+            // line below ensures user cannot go back to login page by repeatedly pressing back
             finish();
         }
 
-        btnLogin = findViewById(R.id.btnLogin);
-        etLoginUsername = findViewById(R.id.etLoginUsername);
-        etLoginPassword = findViewById(R.id.etLoginPassword);
-        tvSignUp = findViewById(R.id.tvSignUp);
+        btnLogin = findViewById(R.id.btnRegister);
+        etLoginUsername = findViewById(R.id.etRegisterUsername);
+        etLoginPassword = findViewById(R.id.etRegisterPassword);
+        tvRegister = findViewById(R.id.tvLogin);
 
-        tvSignUp.setOnClickListener(new View.OnClickListener() {
+        tvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
@@ -79,7 +80,7 @@ public class LoginActivity extends AppCompatActivity {
     private void goToMainActivity(){
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
-        // this is necessary to prevent user from being able to go back to login page by repeatedly clicking back button
+        // line below ensures user cannot go back to login page by repeatedly pressing back
         finish();
     }
 }
