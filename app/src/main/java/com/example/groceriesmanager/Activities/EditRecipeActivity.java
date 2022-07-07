5ae0bc85abbd4c8f08069e37b4d604c44933a448
Mezisashe Ojuba <sasheojuba@fb.com>
2022-07-06 19:20:22 -0700

    partially populated edit recipe layout xml and activity

--- app/src/main/java/com/example/groceriesmanager/Activities/EditRecipeActivity.java
+++ app/src/main/java/com/example/groceriesmanager/Activities/EditRecipeActivity.java
@@ -1,16 +1,84 @@
 package com.example.groceriesmanager.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.groceriesmanager.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EditRecipeActivity extends AppCompatActivity {
+    private EditText etRecipeTitle;
+    private EditText etLink;
+    private CheckBox checkboxVegan;
+    private CheckBox checkboxVegetarian;
+    private CheckBox checkboxGlutenFree;
+    private ImageButton ibAddIngredient;
+    private ImageButton ibAddProcedure;
+    private RecyclerView rvIngredients;
+    private RecyclerView rvProcedure;
+    private Button btnCancel;
+    private Button btnSave;
+    List<String> ingredientList;
+    List<String> procedureList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_recipe);
+
+        etRecipeTitle = findViewById(R.id.etRecipeTitle);
+        etLink = findViewById(R.id.etLink);
+        checkboxGlutenFree = findViewById(R.id.checkboxGlutenFree);
+        checkboxVegan = findViewById(R.id.checkboxVegan);
+        checkboxVegetarian = findViewById(R.id.checkboxVegetarian);
+        ibAddIngredient = findViewById(R.id.ibAddIngredient);
+        ibAddProcedure = findViewById(R.id.ibAddProcedure);
+        btnCancel = findViewById(R.id.btnCancel);
+        btnSave = findViewById(R.id.btnSave);
+        rvIngredients = findViewById(R.id.rvIngredients);
+        rvProcedure = findViewById(R.id.rvProcedure);
+
+        ingredientList = new ArrayList<>();
+        procedureList = new ArrayList<>();
+
+        // todo: set recycler view adapters etc.
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo: check if required fields are filled then save in server
+            }
+        });
+
+        ibAddProcedure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo: check if required fields are filled then add it to the procedure list, update the recycler view
+            }
+        });
+
+        ibAddIngredient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo: check if required fields are filled then add it to the ingredient list, update the recycler view
+            }
+        });
     }
 }
