374ae78b6003feae0206a59eadb8f2ff4c761dca
Mezisashe Ojuba <sasheojuba@fb.com>
2022-07-07 10:11:11 -0700

    user can create and save own recipes, ingredients and procedure added do not show yet

--- app/src/main/java/com/example/groceriesmanager/Activities/EditRecipeActivity.java
+++ app/src/main/java/com/example/groceriesmanager/Activities/EditRecipeActivity.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.groceriesmanager.Models.Recipe;
 import com.example.groceriesmanager.R;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class EditRecipeActivity extends AppCompatActivity {
     private EditText etRecipeTitle;
@@ -22,6 +29,8 @@ public class EditRecipeActivity extends AppCompatActivity {
     private CheckBox checkboxVegan;
     private CheckBox checkboxVegetarian;
     private CheckBox checkboxGlutenFree;
+    private EditText etAddProcedure;
+    private EditText etAddIngredient;
     private ImageButton ibAddIngredient;
     private ImageButton ibAddProcedure;
     private RecyclerView rvIngredients;
@@ -30,6 +39,8 @@ public class EditRecipeActivity extends AppCompatActivity {
     private Button btnSave;
     List<String> ingredientList;
     List<String> procedureList;
+    List<String> filtersList;
+    private static final String TAG = "EditRecipeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +58,12 @@ public class EditRecipeActivity extends AppCompatActivity {
         btnSave = findViewById(R.id.btnSave);
         rvIngredients = findViewById(R.id.rvIngredients);
         rvProcedure = findViewById(R.id.rvProcedure);
+        etAddIngredient = findViewById(R.id.etAddIngredient);
+        etAddProcedure = findViewById(R.id.etAddProcedure);
 
         ingredientList = new ArrayList<>();
         procedureList = new ArrayList<>();
+        filtersList = new ArrayList<>();
 
         // todo: set recycler view adapters etc. here
 
@@ -64,20 +78,80 @@ public class EditRecipeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // todo: check if required fields are filled then save in server
+                String title = etRecipeTitle.getText().toString().trim();
+                if (Objects.equals(title, "")){
+                    Toast.makeText(EditRecipeActivity.this, "Type in a Recipe title.", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Recipe newUserRecipe = new Recipe();
+                    newUserRecipe.setUser(ParseUser.getCurrentUser());
+                    newUserRecipe.setType("user");
+                    newUserRecipe.setTitle(title);
+
+                    String link = etLink.getText().toString().trim();
+                    if (!Objects.equals(link, "")){
+                        newUserRecipe.setHyperlink_url(link);
+                    }
+
+                    if (checkboxVegetarian.isChecked()){
+                        filtersList.add("vegetarian");
+                    }
+                    if (checkboxVegan.isChecked()){
+                        filtersList.add("vegan");
+                    }
+                    if (checkboxGlutenFree.isChecked()){
+                        filtersList.add("gluten-free");
+                    }
+                    if (filtersList.size()!=0){
+                        newUserRecipe.setFilters(filtersList);
+                    }
+
+                    if (ingredientList.size()!=0){
+                        newUserRecipe.setIngredientLines(ingredientList);
+                    }
+
+                    if (procedureList.size()!=0){
+                        newUserRecipe.setProcedure(procedureList);
+                    }
+
+                    newUserRecipe.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e!=null){
+                                Log.e(TAG, "Error saving user's recipe: " + e.toString());
+                                Toast.makeText(EditRecipeActivity.this, "error saving recipe", Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                Toast.makeText(EditRecipeActivity.this, "Recipe successfully saved", Toast.LENGTH_LONG).show();
+                                finish();
+                            }
+                        }
+                    });
+                }
             }
         });
 
         ibAddProcedure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // todo: check if required fields are filled then add it to the procedure list, update the recycler view
+                String step = etAddProcedure.getText().toString().trim();
+                if (!Objects.equals(step, "")){
+                    procedureList.add(step);
+                    etAddProcedure.setText("");
+                    // todo: notify dataset changed when adapter is set
+                }
             }
         });
 
         ibAddIngredient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // todo: check if required fields are filled then add it to the ingredient list, update the recycler view
+                String ingredient = etAddIngredient.getText().toString().trim();
+                if (!Objects.equals(ingredient, "")){
+                    ingredientList.add(ingredient);
+                    etAddIngredient.setText("");
+                    // todo: notify dataset changed when adapter is set
+                }
             }
         });
     }
